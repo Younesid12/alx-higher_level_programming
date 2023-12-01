5316715f65b538e9ed942df911d6fe96ae79af1e
Younesid12 <idyounes95@gmail.com>
2023-11-30 17:52:55 -0800

    prints the number of and the list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if length == 1:
         print("0 arguments.")
     elif length > 1:
-        print("{:d} arguemnt{:s}".format(length-1, "s:" if length > 2 else":"))
+        print("{:d} argument{:s}".format(length-1, "s:" if length > 2 else":"))
         for i in range(1, length):
             print("{:d}: {:s}".format(i, argv[i]))
