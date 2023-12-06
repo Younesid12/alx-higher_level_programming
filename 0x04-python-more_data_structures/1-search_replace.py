8cbd9576aa7a2dbcfa3b4756b8fc043bb63465fd
Younesid12 <idyounes95@gmail.com>
2023-12-06 14:05:16 -0800

    replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,4 +4,4 @@ def search_replace(my_list, search, replace):
     for item in range(len(my_new_list)):
         if my_new_list[item] == search:
             my_new_list[item] = replace
-            return my_new_list
+    return my_new_list
