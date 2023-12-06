a09a238c58987fc0feba693fda09913c686fd5e2
Younesid12 <idyounes95@gmail.com>
2023-12-06 14:01:05 -0800

    replaces all occurrences of an element by another in a new list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    my_new_list = my_list[:]
+    for item in range(len(my_new_list)):
+        if my_new_list[item] == search:
+            my_new_list[item] = replace
+            return my_new_list
