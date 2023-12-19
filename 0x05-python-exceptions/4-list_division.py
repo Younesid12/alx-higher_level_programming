3ba5e6d752f41dee53f120fce5d99fb948803b30
Younesid12 <idyounes95@gmail.com>
2023-12-18 18:42:22 -0800

    divides element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+def list_division(my_list_1, my_list_2, list_length):
+result_list = [0] * list_length
+for i in range(list_length):
+    try:
+        result = my_list_1[i] / my_list_2[i]
+    except TypeError:
+        print("wrong type")
+        result = 0
+    except ZeroDivisionError:
+        print("division by 0")
+        result = 0
+    except IndexError:
+        print("out of range")
+        result = 0
+    finally:
+        result_list[i] = result
+return result_list
