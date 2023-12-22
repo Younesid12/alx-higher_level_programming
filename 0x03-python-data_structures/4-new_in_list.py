2ca84a3e11a86aafc5df44074b8701fa309e8e48
Younesid12 <idyounes95@gmail.com>
2023-12-21 20:29:28 -0800

    replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 and len(my_list) <= idx:
+        return my_list[:]
+    else:
+        my_copied_list = my_list.copy()
+        my_copied_list[idx] = element
+        return my_copied_list
