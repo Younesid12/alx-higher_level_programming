a033177b0723e46162a0b7950cdd32adb5640227
Younesid12 <idyounes95@gmail.com>
2023-12-22 21:54:01 -0800

    prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" " if element != row[-1] else "")
+    for lin_1 in matrix:
+        for colu in lin:
+            print("{:d}".format(col), end=" " if col != lin[-1] else "")
         print()
