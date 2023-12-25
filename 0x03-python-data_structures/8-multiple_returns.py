f27ce3c7b0a24a56935f74eafb2ca1583e658dc9
Younesid12 <idyounes95@gmail.com>
2023-12-24 19:21:37 -0800

    a function that adds 2 tuples.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,5 +2,5 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     new_tuple_a = tuple_a + (0, 0)
     new_tuple_b = tuple_b + (0, 0)
-    new_tuple = tuple_b[0] + tuple_a[0], tuple_a[1] + tuple_b[2]
+    new_tuple = new_tuple_b[0] + new_tuple_a[0], new_tuple_a[1] + new_tuple_b[1]
     return new_tuple
