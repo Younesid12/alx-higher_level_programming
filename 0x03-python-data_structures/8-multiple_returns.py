ef924c06a1a8b9f2330f8f7a45a677271bf86629
Younesid12 <idyounes95@gmail.com>
2023-12-24 19:38:03 -0800

    returns a tuple with the length of a string and its first character.

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    new_tuple_a = tuple_a + (0, 0)
-    new_tuple_b = tuple_b + (0, 0)
-    new_tuple = new_tuple_b[0] + new_tuple_a[0], new_tuple_a[1] + new_tuple_b[1]
-    return new_tuple
+def multiple_returns(sentence):
+    return len(sentence), sentence[0]
