cc5b720662fac3a83933f207a01d669d30232653
Younesid12 <idyounes95@gmail.com>
2024-02-09 21:19:58 -0800

    defines a square and returns the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+class Square:
+    """Class representing a square>"""
+
+    def __init__(self, size=0):
+        """Inizialization of a new square.
+
+        size: must be an integer greater than 0
+        """
+        if type(size) is int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """Returns the current square area."""
+
+        return self.size ** 2
