eb96fda9e5f59a342fe79928d528cb1ae799d5f1
Younesid12 <idyounes95@gmail.com>
2024-02-09 21:36:01 -0800

    defines a square and returns the current square area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Returns the current square area."""
-
         return self.__size ** 2
