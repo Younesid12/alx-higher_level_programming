a173b02d87a682073f448815d0351548b9ef8af9
Younesid12 <idyounes95@gmail.com>
2024-02-09 21:40:07 -0800

    defines a square and returns the current square area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
             size (int): must be an integer greater than 0
         """
-        if type(size) is not int:
+        if isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
