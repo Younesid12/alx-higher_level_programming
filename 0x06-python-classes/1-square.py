187168472201f8a9b978cfe77ad8fcb25efc15b9
Younesid12 <idyounes95@gmail.com>
2024-02-09 17:42:46 -0800

    defines a square by: (based on 0-square.py)

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Defines a square by: (based on 0-square.py)."""
 
 
@@ -7,4 +8,9 @@ class Square:
 
 
     def __init__(self, size):
+        """Initializes a new instance of Square.
+
+        Args:
+            size: The size of the square.
+        """
         self.size = size
