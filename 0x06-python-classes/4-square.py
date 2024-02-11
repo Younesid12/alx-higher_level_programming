195d9394ab25c058cc7f819e2b295effb4a7c61e
Younesid12 <idyounes95@gmail.com>
2024-02-10 22:21:18 -0800

    defines a square by private instance attribute size and setters and getters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,16 +16,16 @@ class Square:
 
     @property
     def size(self):
-            """retrieves the size"""
-            return self.__size
+        """retrieves the size"""
+        return self.__size
     @size.setter
     def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
     def area(self):
         """Returns the current square area"""
         return self.__size ** 2
