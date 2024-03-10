014a946a7cc1a9781da2864df8f0b0b611434da7
Younesid12 <idyounes95@gmail.com>
2024-03-10 11:52:40 +0000

    updated the docsting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,6 +5,7 @@
 
 def add_integer(a, b=98):
 
+
 	""" this functions adds two integers
 	a and b must be integers or floats otherwise a TypeError would raise
 	examples:
@@ -35,4 +36,5 @@ def add_integer(a, b=98):
 		a = int(a)
 	elif isinstance(b, float):
 		b = int(b)
+
 	return a + b
