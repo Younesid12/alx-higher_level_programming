d2ed18056e5e5bec002470e6e7e0dad6ea896d4a
Younesid12 <idyounes95@gmail.com>
2024-03-10 12:02:04 +0000

    updated the docsting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,29 +5,36 @@
 
 def add_integer(a, b=98):
 
+	""" this functions adds two integers.
 
-	""" this functions adds two integers
-	a and b must be integers or floats otherwise a TypeError would raise
-	examples:
-	
-	>>> add_integer(1, 2)
-	3
-	>>> add_integer(100, -2)
-	98
-	>>> add_integer(2)
-	100
-	>>> add_integer(100.3, -2)
-	98
-	>>> add_integer(4, 'School')
-	Traceback (most recent call last):
-	...
-	TypeError: b must be an integer
-	>>> add_integer(None)
-	Traceback (most recent call last):
-	...
-	TypeError:a must be an integer
-	"""
+	Args:
+		a (int or floar): the first number.
+		b (int or float): the second number (default is 98).
 
+	Returns:
+		int: the sum of a and b.
+
+	Raises:
+		TypeError: if a or b is not an integer or float.
+
+	Examples:
+		>>> add_integer(1, 2)
+		3
+		>>> add_integer(100, -2)
+		98
+		>>> add_integer(2)
+		100
+		>>> add_integer(100.3, -2)
+		98
+		>>> add_integer(4, 'School')
+		Traceback (most recent call last):
+		...
+		TypeError: b must be an integer
+		>>> add_integer(None)
+		Traceback (most recent call last):
+		...
+		TypeError:a must be an integer
+		"""
 	if not isinstance(a, (int, float)):
 		raise TypeError("a must be an integer")
 	elif not isinstance(b, (int, float)):
