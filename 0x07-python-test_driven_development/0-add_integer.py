b182d3024b78244d13936e03ce2529424188ad7f
Younesid12 <idyounes95@gmail.com>
2024-03-10 11:41:38 +0000

    updated the docsting

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,10 +13,18 @@ def add_integer(a, b=98):
 	3
 	>>> add_integer(100, -2)
 	98
+	>>> add_integer(2)
+	100
+	>>> add_integer(100.3, -2)
+	98
 	>>> add_integer(4, 'School')
 	Traceback (most recent call last):
 	...
 	TypeError: b must be an integer
+	>>> add_integer(None)
+	Traceback (most recent call last):
+	...
+	TypeError:a must be an integer
 	"""
 
 	if not isinstance(a, (int, float)):
