ed7d821e1c91031841981bce76b60b9fb4e38d29
Younesid12 <idyounes95@gmail.com>
2024-03-13 00:22:52 -0700

    adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -33,7 +33,7 @@ def add_integer(a, b=98):
 	Traceback (most recent call last):
 	...
 	TypeError: a must be an integer
-    """
+        """
     if not isinstance(a, (int, float)):
 	    raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
