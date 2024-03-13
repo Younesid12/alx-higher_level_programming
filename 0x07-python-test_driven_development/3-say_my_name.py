dae7fca74f8eaaf96dba870c7332f15144bed1db
Younesid12 <idyounes95@gmail.com>
2024-03-13 05:06:39 -0700

    prints the full name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+
     """
     prints the fullname.
 
@@ -36,4 +37,3 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
-
