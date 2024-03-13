86951573bdee7751cf714db445fc14e4bb0d2344
Younesid12 <idyounes95@gmail.com>
2024-03-13 00:37:03 -0700

    adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,36 +3,37 @@
 """Define a faunction of addition of integers"""
 
 
-def add_integer(a, b=98):   
-   """
-    This function adds two integers.
+def add_integer(a, b=98):
+    """
+    a functionn that Adds two integers.
 
     Args:
-	    a (int or float): The first number.
-	    b (int or float): The second number (default is 98).
+        a (int or float): The first number.
+        b (int or float): The second number (default is 98).
+
     Returns:
-            int: The sum of a and b.
+        int: The sum of a and b.
 
     Raises:
-	    TypeError: If a or b is not an integer or float.
+        TypeError: If a or b is not an integer or float.
 
     Examples:
         >>> add_integer(1, 2)
-	3
-	>>> add_integer(100, -2)
-	98
-	>>> add_integer(2)
-	100
-	>>> add_integer(100.3, -2)
-	98
-	>>> add_integer(4, "School")
-	Traceback (most recent call last):
-	...
-	TypeError: b must be an integer
-	>>> add_integer(None)
-	Traceback (most recent call last):
-	...
-	TypeError: a must be an integer
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2)
+        100
+        >>> add_integer(100.3, -2)
+        98
+        >>> add_integer(4, "School")
+        Traceback (most recent call last):
+        ...
+        TypeError: b must be an integer
+        >>> add_integer(None)
+        Traceback (most recent call last):
+        ...
+        TypeError: a must be an integer
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
