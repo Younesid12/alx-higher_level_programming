bfbd639aa87bcf21f3b8b2925316c533da90b17c
Younesid12 <idyounes95@gmail.com>
2024-03-15 20:38:12 -0700

    updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,20 @@
 
 
 import unittest
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
+
 class TestMaxInteger(unittest.TestCase):
 
     """ A Test Call that defins some tests for max_integer(list=[]): """
@@ -30,4 +44,4 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, -3]), 2)
     def test_one_elem(self):
         """ tests the function when it recieves a list of only one element"""
-        self.assertEqual([2], 2)
+        self.assertEqual(max_integer([2]), 2)
