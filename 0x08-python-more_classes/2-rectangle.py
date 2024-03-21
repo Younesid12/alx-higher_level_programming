44d7a8b50e3c985e885317310858af687773139a
Younesid12 <idyounes95@gmail.com>
2024-03-21 01:30:08 -0700

    added two methods , area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -38,7 +38,7 @@ class Rectangle():
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
-    
+
     def area(self):
         """Returns the area of a treangle"""
         return self.__width * self.__height
