18b4c2a1f31bad86739d60a1e83d83e59be0035e
Younesid12 <idyounes95@gmail.com>
2024-03-21 01:28:28 -0700

    added two methods , area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle():
     @width.setter
     def width(self, value):
         """sets a value to width"""
-        if type(width) != int:
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -32,7 +32,7 @@ class Rectangle():
     @height.setter
     def height(self, value):
         """ sets the height attr"""
-        if type(height) != int:
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
