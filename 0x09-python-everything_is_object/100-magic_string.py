d22b7b849e615a9d783726c0a22313def8a63836
Younesid12 <idyounes95@gmail.com>
2024-03-25 06:32:55 -0700

    function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.time = getattr(magic_string, 'time', 1) + 1
+    magic_string.time = getattr(magic_string, 'time', 1)
     return ", ".join(['BestSchool'] * magic_string.time)
