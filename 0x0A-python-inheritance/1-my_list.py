af63b53700bf0ac04079b3a86c868cbb41c4a1d1
Younesid12 <idyounes95@gmail.com>
2024-04-13 22:05:52 -0700

    defines a class that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" defines a class named my_list"""
+
+
+class MyList:
+    """list-inherited class"""
+    def print_sorted(self):
+        """prints the list  in a sorted manner"""
+        print(sorted(self))
