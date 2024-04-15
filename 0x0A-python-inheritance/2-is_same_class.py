0785fc838b24fe3600be9956ffad675e5f4dd6ec
Younesid12 <idyounes95@gmail.com>
2024-04-14 19:13:30 -0700

    returns True if the object is exactly an instance of the specified class ; otherwise False.

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -6,6 +6,6 @@
 def is_same_class(obj, a_class):
     """ returns true if the object is exactly an instance of the specified class
     otherwise false"""
-    if type(obj) == a_class):
+    if type(obj) == a_class:
         return True
     return False
