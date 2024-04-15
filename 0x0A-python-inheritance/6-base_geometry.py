42399c61de6e68aa49617777066f26252e4f9925
Younesid12 <idyounes95@gmail.com>
2024-04-15 10:24:37 -0700

    class BaseGeometry, defining area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ defines a BaseGeometry class"""
+    def area(self):
+        """raises an Exception with the message area() is not complicated"""
+        raise Exception("area() is not implemented")
