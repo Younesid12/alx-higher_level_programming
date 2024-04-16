eab5c1df3096ca545157f405299366df72d4c0e8
Younesid12 <idyounes95@gmail.com>
2024-04-15 20:05:33 -0700

    test

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(n):
     """Rectangle class"""
     def __init__(self, width, height):
         """initializes instance attributes"""
-        Rectangle.integer_validator("height", height)
-        Rectangle.integer_validator("width", width)
+        n.integer_validator("height", height)
+        n.integer_validator("width", width)
         self.__width = width
         self.__height = height
