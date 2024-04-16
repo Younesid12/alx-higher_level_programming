efe4ba252b7a7bf00f3a21c27c05daef066a6330
Younesid12 <idyounes95@gmail.com>
2024-04-15 18:10:49 -0700

    defines BaseGeometry class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,7 +2,7 @@
 """defines a class BaseGeometry"""
 
 
-class BaseGeometry:
+class BaseGeometry():
     """ defines a BaseGeometry class"""
     def area(self):
         """raises an Exception with the message area() is not complicated"""
