68ddc23c8d5640baa513142cb8307cbb38d1dd07
Younesid12 <idyounes95@gmail.com>
2024-04-15 19:05:40 -0700

    updated

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,6 +7,7 @@ class BaseGeometry:
     def area(self):
         """raises an Exception with the message area() is not complicated"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """validates value"""
         if type(value) is not int:
