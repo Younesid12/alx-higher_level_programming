9f1052367db54391f2f80c63ceecda4486eeeaad
Younesid12 <idyounes95@gmail.com>
2024-04-15 17:56:58 -0700

    defines BaseGeometry class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
     def area(self):
         """raises an Exception with the message area() is not complicated"""
         raise Exception("area() is not implemented")
-    def integer_validator(slef, name, value):
+    def integer_validator(self, name, value):
         """validates value"""
         if type(value) is not int:
             raise TypeError(f"{name] must be an integer")
