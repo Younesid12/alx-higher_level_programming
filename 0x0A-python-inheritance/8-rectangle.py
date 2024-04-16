3e9441762b910c45270c2c9f32f604363c6bf7a8
Younesid12 <idyounes95@gmail.com>
2024-04-15 20:09:09 -0700

    test

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,13 +2,11 @@
 """ Defines a Rectangle class"""
 
 
-n = __import__("7-base_geometry.py").BaseGeometry
-
-
 class Rectangle(n):
     """Rectangle class"""
     def __init__(self, width, height):
         """initializes instance attributes"""
+        n = __import__("7-base_geometry.py").BaseGeometry
         n.integer_validator("height", height)
         n.integer_validator("width", width)
         self.__width = width
