6007850667e28f9a1121762b2bf6c73bd2b7007e
Younesid12 <idyounes95@gmail.com>
2024-04-15 18:13:43 -0700

    defines BaseGeometry class

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -2,7 +2,7 @@
 """defines a class BaseGeometry"""
 
 
-class BaseGeometry():
+class BaseGeometry:
     """ defines a BaseGeometry class"""
     def area(self):
         """raises an Exception with the message area() is not complicated"""
@@ -10,6 +10,6 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """validates value"""
         if type(value) is not int:
-            raise TypeError(f"{name] must be an integer")
+            raise TypeError(f"{name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{name} must be greater than 0")
