9f758daedc9729b3e461dbf5400e008452df4ca0
Younesid12 <idyounes95@gmail.com>
2024-04-15 23:42:58 -0700

    a class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py).

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""defines a class a class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class definition"""
+    def __init__(self, size):
+        """initializes size"""
+        try:
+            super().__init__(size, size)
+        except ValueError:
+            raise ValueError("size must be greater than 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+        else:
+            self.__size = size
+    
+    def __str__(self):
+        returns f"[Square] {self.__size}/{self.__size}"
