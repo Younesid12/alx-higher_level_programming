e0b7d2ca331089ca4cbac9297d2257d113f4bf07
Younesid12 <idyounes95@gmail.com>
2024-04-18 23:12:05 -0700

    reads from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Reads a file"""
-    with open(filename, 'r', encoding='UTF-8') as n:
+    with open(filename, encoding='utf-8') as n:
         content = n.read()
     print(content)
