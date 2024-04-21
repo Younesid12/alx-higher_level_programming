43b3c71235f490c30e92a2655c5ca985d71f88a6
Younesid12 <idyounes95@gmail.com>
2024-04-20 22:34:15 -0700

    updated

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""defines a class student"""
+
+
+class Student:
+    """ class Student that defines a studen"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary
+        representation of a Student instance"""
+        data = {}
+        if (isinstance(attrs, list) and
+                all(isinstance(element, str) for element in attrs)):
+            for value in attrs:
+                if value in self.__dict__:
+                    data[value] = self.__dict__[value]
+            return data
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except AttributeError:
+                pass
