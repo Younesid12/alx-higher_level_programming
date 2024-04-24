8b7c2eb5fe5bbeb08503a30a744fb012d6369d74
Younesid12 <idyounes95@gmail.com>
2024-04-24 01:28:26 -0700

    updated the rectangle class with display method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,4 +97,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns a string"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/\
+                {self.__y} - {self.__width}/{self.__height}"
