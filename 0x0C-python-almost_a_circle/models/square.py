e216ff81b085bd3efc488e75fb90d5fbe678475e
Younesid12 <idyounes95@gmail.com>
2024-04-24 23:46:54 -0700

    square class that inherits from rectangle class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     """square class that inherits from Rectangle superclass"""
     def __init__(self, size, x=0, y=0, id=None):
         """initialized the attributes"""
-        super().__init__(size, size, x, x, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """ should return [Square] (<id>) <x>/<y> - <size>"""
