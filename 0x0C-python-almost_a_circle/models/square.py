8e911da25efff3d50160394481d7b7c90e2f0bd5
Younesid12 <idyounes95@gmail.com>
2024-04-25 01:17:14 -0700

    updated square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,7 +12,7 @@ class Square(Rectangle):
 
     def __str__(self):
         """ should return [Square] (<id>) <x>/<y> - <size>"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
@@ -23,3 +23,26 @@ class Square(Rectangle):
     def size(self, size):
         """sets the size"""
         self.width = size
+
+    def update(self, *args, **kwargs):
+        """assigns attributes"""
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == 'size':
+                    self.size = value
+                elif key == 'x':
+                    self.x = value
+                elif key == 'y':
+                    self.y = value
+        else:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.size = args[i]
+                elif i == 2:
+                    self.x = args[i]
+                elif i == 3:
+                    self.y = args[i]
