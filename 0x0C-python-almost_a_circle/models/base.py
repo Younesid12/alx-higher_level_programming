01051ccb604269ee0f72f8a9b2299aaf3fb0596c
Younesid12 <idyounes95@gmail.com>
2024-04-25 18:52:37 -0700

    updated

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,8 +28,8 @@ class Base:
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
         for i in range(len(list_objs)):
-            with open(f"{type(list_objes[i])}.json", 'w',\
-                    encoding='UTF-8') as n:
+            filenamw = f"{type(list_objes[i].json}"
+            with open(filename, 'w', encoding='UTF-8') as n:
                 n.write(to_json_string())
 
     @staticmethod
